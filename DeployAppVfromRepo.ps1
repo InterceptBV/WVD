52c97e9398de3a1a5ae973174ac5dd5bd335d3d2
RinieH <62855963+RinieH@users.noreply.github.com>
2020-09-08 10:41:57 +0200

    Add files via upload

---
+++ DeployAppVfromRepo.ps1
@@ -0,0 +1,24 @@
+﻿$PATfile = 'C:\Git\PAT'
+$LocalRepositoryPath = 'c:\App-V'
+$StudentAppVPath = $LocalRepositoryPath+'\REPO\PATH\APPVNAME.appv'
+
+$PAT = Get-Content -Path $PATfile
+
+set-location $LocalRepositoryPath
+if (Test-Path -Path ($LocalRepositoryPath+'\REPO'))
+    {
+     set-location .\PATH
+     git fetch
+     git merge -X theirs
+    }
+else 
+   {
+     git clone https://$PAT@dev.azure.com/interceptvsts/RELEASE --branch BRANCHNAME --single-branch BRANCHNAME
+    }
+
+Get-AppvClientPackage | Remove-AppvClientPackage 
+$Package = Add-AppvClientPackage -Path $StudentAppVPath | Publish-AppvClientPackage -Global
+Mount-AppvClientPackage -PackageId $Package.PackageId -VersionId $Package.VersionId
+
+remove-item 'C:\Users\Public\Desktop\*ISVNAME*'
+copy-item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\APPNAME\*ISVNAME*' 'C:\Users\Public\Desktop\'
---
+++ deploy_CreateHostpoolTemplate.ps1
@@ -0,0 +1,9 @@
+# Edit template parameters
+$templateFile = "https://raw.githubusercontent.com/RinieH/WVD/master/CreateHostpoolTemplate.ps1"
+
+#Deploy from Git template
+#https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deploy-to-azure-button
+$EncodedURI = [uri]::EscapeDataString($templateFile)
+
+Start "https://portal.azure.com/#create/Microsoft.Template/uri/$EncodedURI"
+
